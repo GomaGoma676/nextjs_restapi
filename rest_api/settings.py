6d5fdc9e03445a45caf230cf333f29da493b40ea
GomaGoma676 <pga.adam.scott@gmail.com>
2020-12-30 02:06:49 +0900

    added host url

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nextjs-api.herokuapp.com']
 
 
 # Application definition
